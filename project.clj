c263606cb3dbeb2e1fcb44e9b5bd33f16d822e46
Aseem Chopra <aseemchopra@protonmail.com>
2024-03-31 00:02:08 +0530

    update: dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,6 @@
 (defproject stashpad "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
-  :main ^:skip-aot stashpad.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+  :description "A simple Clojure-based pastebin"
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [ring "1.8.2"]
+                 [compojure "1.6.1"]]
+  :main ^:skip-aot stashpad.core)
