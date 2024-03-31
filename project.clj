96bce62a705f7e504c0498c7484cdd61c8a0627a
Aseem Chopra <aseemchopra@protonmail.com>
2024-03-31 22:51:42 +0530

    first working code iteration

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "A simple Clojure-based pastebin"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring "1.8.2"]
+                 [ring/ring-defaults "0.3.3"]
                  [compojure "1.6.1"]]
   :main ^:skip-aot stashpad.core)
--- src/stashpad/core.clj
+++ src/stashpad/core.clj
@@ -1,7 +1,48 @@
 (ns stashpad.core
-  (:gen-class))
+  (:require [compojure.core :refer [defroutes GET POST]]
+            [compojure.route :as route]
+            [ring.adapter.jetty :as jetty]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.params :refer [wrap-params]]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def snippets (atom {})) ;; atoms would prevent race conditions
+
+(defn save-snippet [snippet]
+  (let [id (str (java.util.UUID/randomUUID))]
+    (swap! snippets assoc id snippet)
+    (println "Saved snippet with ID:" id "Content:" snippet) ;; content keeps showing nil
+    id))
+
+(defn get-snippet [id]
+  (@snippets id))
+
+
+(defn submit-snippet [request]
+  (let [params-map (:params request)
+        snippet-from-direct-access (:snippet params-map)]
+    (println "Params Map:" params-map)
+    (println "Snippet from Direct Access:" snippet-from-direct-access)
+    (let [id (save-snippet snippet-from-direct-access)]
+      (println "Before response, Snippet:" snippet-from-direct-access) ;; Confirm it's still not nil.
+      (str "Saved snippet with ID: " id " <a href='/snippets/" id "'>View</a>"))))
+
+
+
+(defroutes app-routes
+  (GET "/" request
+    (let [token (:anti-forgery-token request)]
+      (str "<form method='POST' action='/submit'>"
+           "<input type='hidden' name='__anti-forgery-token' value='" token "'/>"
+           "<input type='text' name='snippet'/>"
+           "<input type='submit' value='Submit'/>"
+           "</form>")))
+  (POST "/submit" request (submit-snippet request))
+  (GET "/snippets/:id" [id] (str "<h2>Snippet:</h2><p>" (get-snippet id) "</p>"))
+  (route/not-found "Not Found"))
+
+(def app
+  (-> (wrap-defaults app-routes site-defaults)
+      (wrap-params)))
+
+(defn -main []
+  (jetty/run-jetty app {:port 3000 :join? false}))
